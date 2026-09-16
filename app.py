15f835029422ddd1cf87050f7eb6e35e44572ad4
TaxFraud0709 <austinc114@nycstudents.net>
2026-09-16 07:32:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import turtle
-from turtle import *
-t = Turtle()
+# import turtle
+# from turtle import *
+# t = Turtle()
 
-t.shape('turtle')
+# t.shape('turtle')
 
 # def square(x,y):
 #     for i in range(4):
@@ -20,19 +20,19 @@ t.shape('turtle')
 
 
 
-def star(x,y):
-    for i in range(5):
-        t.forward(x)
-        t.right(y)
-        t.forward(x)
-        t.right(y)
-        t.forward(x)
-        t.right(y)
-        t.forward(x)
-        t.right(y)
-        t.forward(x)
+# def star(x,y):
+#     for i in range(5):
+#         t.forward(x)
+#         t.right(y)
+#         t.forward(x)
+#         t.right(y)
+#         t.forward(x)
+#         t.right(y)
+#         t.forward(x)
+#         t.right(y)
+#         t.forward(x)
 
-star(100,144)
+# star(100,144)
 
 # def rotate():
 #     length = 5
@@ -42,3 +42,30 @@ star(100,144)
 #         length += 5
 #         t.left(5)
 # rotate()
+
+
+def trees():
+    treelength = input()
+    trees = list(input().split())
+    greater = 0
+    greatersample = 1
+    lesser = 0
+    lessersample = 1
+
+    for i in range(len(trees)):
+        
+        if trees[i] > trees[i-1]:
+            greatersample += 1  
+        if greatersample > greater:
+            greater = greatersample
+        else:
+            greatersample = 1
+        if trees[i] < trees[i-1]:
+            lessersample += 1
+        if lessersample > lesser:
+            lesser = lessersample
+        else:
+            lessersample = 1
+    print(greater)
+    print(lesser)
+trees()
